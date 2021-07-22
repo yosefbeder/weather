b08a1dd4bfe943c23d636827209896aaf0bbd1c5
yosefbeder <dryosefbeder@gmail.com>
2021-07-22 08:10:05 +0200

    Fixed the routing glitch, and changed the title and the icon

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import store from './store';
 
 ReactDOM.render(
